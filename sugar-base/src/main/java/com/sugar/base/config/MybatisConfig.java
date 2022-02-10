041a8259d37550cd0010dffac451a2db65e97e03
sugar <sugar_tech@outlook.com>
2022-02-10 18:47:34 +0800

    [update] sugar-lost 逻辑删除插件

--- sugar-base/src/main/java/com/sugar/base/config/MybatisConfig.java
+++ sugar-base/src/main/java/com/sugar/base/config/MybatisConfig.java
@@ -1,5 +1,7 @@
 package com.sugar.base.config;
 
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,5 +11,10 @@ public class MybatisConfig {
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
+    }
+
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
     }
 }
