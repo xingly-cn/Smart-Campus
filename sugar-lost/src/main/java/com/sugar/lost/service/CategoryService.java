e580861b2e51c006376c643cdc9cb640af4d8d72
sugar <sugar_tech@outlook.com>
2022-02-10 16:06:25 +0800

    [update] sugar-lost 物品分类

--- sugar-lost/src/main/java/com/sugar/lost/service/CategoryService.java
+++ sugar-lost/src/main/java/com/sugar/lost/service/CategoryService.java
@@ -13,4 +13,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface CategoryService extends IService<Category> {
 
+    boolean editCategory(Category category);
 }
--- sugar-lost/src/main/java/com/sugar/lost/service/impl/CategoryServiceImpl.java
+++ sugar-lost/src/main/java/com/sugar/lost/service/impl/CategoryServiceImpl.java
@@ -1,10 +1,13 @@
 package com.sugar.lost.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.sugar.lost.entity.Category;
 import com.sugar.lost.mapper.CategoryMapper;
 import com.sugar.lost.service.CategoryService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * <p>
@@ -17,4 +20,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
 
+    public boolean editCategory(Category category) {
+        String id = category.getId();
+        int success = baseMapper.updateById(category);
+        return id.isEmpty() ? false : success > 0 ? true : false ;
+    }
 }
