f5189c4984a20f153643181e4e62fad61eaef313
sugar <sugar_tech@outlook.com>
2022-02-10 16:06:12 +0800

    [update] sugar-lost 分页

--- sugar-lost/src/main/java/com/sugar/lost/controller/CategoryController.java
+++ sugar-lost/src/main/java/com/sugar/lost/controller/CategoryController.java
@@ -1,9 +1,15 @@
 package com.sugar.lost.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.sugar.base.result.R;
+import com.sugar.lost.entity.Category;
+import com.sugar.lost.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -15,7 +21,47 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/lost/category")
+@CrossOrigin
+@Api(tags = "分类管理")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/getList")
+    @ApiOperation("分类列表")
+    public R getList(long cur, long size) {
+        Page<Category> page = new Page<Category>(cur,size);
+        QueryWrapper<Category> wrapper = new QueryWrapper<Category>();
+        wrapper.orderByDesc("level");
+        categoryService.page(page,wrapper);
+        long total = page.getTotal();
+        return R.ok().data("categoryList",page.getRecords()).data("total",total);
+    }
+
+    @GetMapping("getInfo")
+    @ApiOperation("分类信息")
+    public R getInfo(String categoryId) {
+        return R.ok().data("category",categoryService.getById(categoryId));
+    }
+
+    @PostMapping("add")
+    @ApiOperation("添加分类")
+    public R add(@RequestBody Category category) {
+        return R.ok().data("success",categoryService.save(category));
+    }
+
+    @PostMapping("del")
+    @ApiOperation("删除分类")
+    public R del(String categoryId) {
+        return R.ok().data("success",categoryService.removeById(categoryId));
+    }
+
+    @PostMapping("edit")
+    @ApiOperation("编辑分类")
+    public R edit(@RequestBody Category category) {
+        return R.ok().data("success",categoryService.editCategory(category));
+    }
 
 }
 
