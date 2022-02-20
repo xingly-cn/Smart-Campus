4273d8eec68c0107ed8921dea574fd76dd46604e
肖念昕 <sugar_tech@outlook.com>
2022-02-20 23:09:06 +0800

    [update]

--- sugar-lost/src/test/java/com/sugar/CodeGenerator.java
+++ sugar-lost/src/test/java/com/sugar/CodeGenerator.java
@@ -10,74 +10,74 @@ import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import org.junit.jupiter.api.Test;
+
 
 /**
  * 逆向工程
  */
 public class CodeGenerator {
-
-    @Test
-    public void run() {
-
-        // 1、创建代码生成器
-        AutoGenerator mpg = new AutoGenerator();
-
-        // 2、全局配置
-        GlobalConfig gc = new GlobalConfig();
-        gc.setOutputDir("C:\\Users\\98759\\Desktop\\Smart-Campus\\sugar-lost" + "/src/main/java"); //输出目录
-
-        gc.setAuthor("方糖"); //作者名
-        gc.setOpen(false); //生成后是否打开资源管理器
-        gc.setFileOverride(false); //重新生成时文件是否覆盖
-
-        gc.setServiceName("%sService");	//去掉Service接口的首字母I
-        gc.setIdType(IdType.ID_WORKER_STR); //主键策略
-        gc.setDateType(DateType.ONLY_DATE);//定义生成的实体类中日期类型
-        gc.setSwagger2(true);//开启Swagger2模式
-
-        mpg.setGlobalConfig(gc);
-
-        // 3、数据源配置
-        DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://8.142.199.134:3306/eschool?serverTimezone=GMT%2B8");
-        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
-        dsc.setUsername("root");
-        dsc.setPassword("XNXxnx520");
-        dsc.setDbType(DbType.MYSQL);
-        mpg.setDataSource(dsc);
-
-        // 4、包配置
-        PackageConfig pc = new PackageConfig();
-
-        //生成包
-        pc.setParent("com.sugar");
-        pc.setModuleName("lost"); //模块名
-
-        //生成包：com.asugar.edu.controller
-        pc.setController("controller");
-        pc.setEntity("entity");
-        pc.setService("service");
-        pc.setMapper("mapper");
-        mpg.setPackageInfo(pc);
-
-        // 5、策略配置
-        StrategyConfig strategy = new StrategyConfig();
-        strategy.setInclude("lost_good","lost_inform","lost_callback","lost_msg","lost_school","lost_stu");//根据数据库哪张表生成，有多张表就加逗号继续填写
-
-        strategy.setNaming(NamingStrategy.underline_to_camel);//数据库表映射到实体的命名策略
-        strategy.setTablePrefix(pc.getModuleName() + "_"); //生成实体时去掉表前缀
-
-        strategy.setColumnNaming(NamingStrategy.underline_to_camel);//数据库表字段映射到实体的命名策略
-        strategy.setEntityLombokModel(true);
-
-        strategy.setRestControllerStyle(true); //restful api风格控制器
-        strategy.setControllerMappingHyphenStyle(true); //url中驼峰转连字符
-
-        mpg.setStrategy(strategy);
-
-
-        // 6、执行
-        mpg.execute();
-    }
+    //
+    //@Test
+    //public void run() {
+    //
+    //    // 1、创建代码生成器
+    //    AutoGenerator mpg = new AutoGenerator();
+    //
+    //    // 2、全局配置
+    //    GlobalConfig gc = new GlobalConfig();
+    //    gc.setOutputDir("C:\\Users\\98759\\Desktop\\Smart-Campus\\sugar-lost" + "/src/main/java"); //输出目录
+    //
+    //    gc.setAuthor("方糖"); //作者名
+    //    gc.setOpen(false); //生成后是否打开资源管理器
+    //    gc.setFileOverride(false); //重新生成时文件是否覆盖
+    //
+    //    gc.setServiceName("%sService");	//去掉Service接口的首字母I
+    //    gc.setIdType(IdType.ID_WORKER_STR); //主键策略
+    //    gc.setDateType(DateType.ONLY_DATE);//定义生成的实体类中日期类型
+    //    gc.setSwagger2(true);//开启Swagger2模式
+    //
+    //    mpg.setGlobalConfig(gc);
+    //
+    //    // 3、数据源配置
+    //    DataSourceConfig dsc = new DataSourceConfig();
+    //    dsc.setUrl("jdbc:mysql://8.142.199.134:3306/eschool?serverTimezone=GMT%2B8");
+    //    dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+    //    dsc.setUsername("root");
+    //    dsc.setPassword("XNXxnx520");
+    //    dsc.setDbType(DbType.MYSQL);
+    //    mpg.setDataSource(dsc);
+    //
+    //    // 4、包配置
+    //    PackageConfig pc = new PackageConfig();
+    //
+    //    //生成包
+    //    pc.setParent("com.sugar");
+    //    pc.setModuleName("lost"); //模块名
+    //
+    //    //生成包：com.asugar.edu.controller
+    //    pc.setController("controller");
+    //    pc.setEntity("entity");
+    //    pc.setService("service");
+    //    pc.setMapper("mapper");
+    //    mpg.setPackageInfo(pc);
+    //
+    //    // 5、策略配置
+    //    StrategyConfig strategy = new StrategyConfig();
+    //    strategy.setInclude("lost_good","lost_inform","lost_callback","lost_msg","lost_school","lost_stu");//根据数据库哪张表生成，有多张表就加逗号继续填写
+    //
+    //    strategy.setNaming(NamingStrategy.underline_to_camel);//数据库表映射到实体的命名策略
+    //    strategy.setTablePrefix(pc.getModuleName() + "_"); //生成实体时去掉表前缀
+    //
+    //    strategy.setColumnNaming(NamingStrategy.underline_to_camel);//数据库表字段映射到实体的命名策略
+    //    strategy.setEntityLombokModel(true);
+    //
+    //    strategy.setRestControllerStyle(true); //restful api风格控制器
+    //    strategy.setControllerMappingHyphenStyle(true); //url中驼峰转连字符
+    //
+    //    mpg.setStrategy(strategy);
+    //
+    //
+    //    // 6、执行
+    //    mpg.execute();
+    //}
 }
